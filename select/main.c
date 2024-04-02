81e80d9cf6904f6cfd22a7440c613b4b10772866
John Hamilton <john@thegrizzledcoder.com>
2024-04-01 17:23:38 -0700

    - Removed unused #includes

--- select/main.c
+++ select/main.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/select.h>
-#include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
